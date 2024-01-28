4476d3c12432ec1f7d9d3c89c619c6d99a6f0114
Alex Beeston <Abeeston@DHICorp.com>
2024-01-27 22:06:04 -0700

    add default value property to input-field-component; write initial calculations for mortage payment schedule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,11 @@ import { InputFieldComponent } from './input-field/input-field.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InputFieldComponent ],
+  imports: [CommonModule, RouterOutlet, InputFieldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   testProperty: string = "testing";
   title = 'prepaymentCalculator';
@@ -19,7 +20,41 @@ export class AppComponent {
   interestRate: number = 0;
 
   stringToNumber(stringLiteral: string): number {
-    console.log("parsing" + stringLiteral);
     return Number(stringLiteral);
   }
+
+  computePaymentSchedule(): LoanLifespan {
+    let currentBalance: number = this.initialBalance;
+    const lifespan: LoanLifespan = new LoanLifespan();
+    while (currentBalance > 0) {
+      const interestComponent: number = currentBalance * this.interestRate / 12;
+      const principalComponent: number = Math.min(this.monthlyPayment - interestComponent, currentBalance);
+
+      lifespan.monthsToPay++;
+      lifespan.totalInterestPaid += interestComponent;
+      currentBalance -= principalComponent;
+    }
+
+    return lifespan;
+  }
+
+  logLifespan(lifespan: LoanLifespan) {
+    let USDollar = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+    console.log("Total interest: " + lifespan.totalInterestPaid);
+    console.log("Months to finish: " + lifespan.monthsToPay);
+  }
+}
+
+class LoanLifespan {
+  constructor() {
+    this.totalInterestPaid = 0;
+    this.monthsToPay = 0;
+  }
+
+  totalInterestPaid: number;
+  monthsToPay: number;
 }
--- src/app/input-field/input-field.component.ts
+++ src/app/input-field/input-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-field',
@@ -8,9 +8,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './input-field.component.scss'
 })
 
-export class InputFieldComponent {
+export class InputFieldComponent implements OnInit {
+  ngOnInit(): void {
+    this.emitter.emit(this.defaultValue);
+  }
+
   @Input() nameAttribute!: string;
   @Input() label!: string;
   @Input() helpText!: string;
+  @Input() defaultValue!: string;
   @Output() emitter: EventEmitter<string> = new EventEmitter<string>();
 }
