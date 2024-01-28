97c5719d4a8bef481c2b64a747dc0179b30dad4a
Alex Beeston <Abeeston@DHICorp.com>
2024-01-27 22:27:06 -0700

    add results table at bottom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,23 +12,27 @@ import { InputFieldComponent } from './input-field/input-field.component';
 })
 
 export class AppComponent {
-  testProperty: string = "testing";
-  title = 'prepaymentCalculator';
-
   initialBalance: number = 0;
   monthlyPayment: number = 0;
   interestRate: number = 0;
+
+  loanWithoutExtraPayment: LoanLifespan = new LoanLifespan();
+  loanWithExtraPrincipal: LoanLifespan = new LoanLifespan();
 
   stringToNumber(stringLiteral: string): number {
     return Number(stringLiteral);
   }
 
-  computePaymentSchedule(): LoanLifespan {
-    let currentBalance: number = this.initialBalance;
+  updateLifespans(): void {
+    this.loanWithExtraPrincipal = this.computeLifespan(this.initialBalance, this.monthlyPayment, this.interestRate);
+  }
+
+  computeLifespan(initialBalance: number, monthlyPayment: number, interestRate: number): LoanLifespan {
+    let currentBalance: number = initialBalance;
     const lifespan: LoanLifespan = new LoanLifespan();
     while (currentBalance > 0) {
-      const interestComponent: number = currentBalance * this.interestRate / 12;
-      const principalComponent: number = Math.min(this.monthlyPayment - interestComponent, currentBalance);
+      const interestComponent: number = currentBalance * interestRate / 12;
+      const principalComponent: number = Math.min(monthlyPayment - interestComponent, currentBalance);
 
       lifespan.monthsToPay++;
       lifespan.totalInterestPaid += interestComponent;
@@ -38,14 +42,15 @@ export class AppComponent {
     return lifespan;
   }
 
-  logLifespan(lifespan: LoanLifespan) {
-    let USDollar = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-    });
+  currencyFormatter: Intl.NumberFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
 
-    console.log("Total interest: " + lifespan.totalInterestPaid);
-    console.log("Months to finish: " + lifespan.monthsToPay);
+  formatNumMonthsAsString(totalMonths: number): string {
+    const numYears = Math.floor(totalMonths / 12);
+    const numMonths = totalMonths % 12;
+    return `${numYears} years, ${numMonths} months`;
   }
 }
 
